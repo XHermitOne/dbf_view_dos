0aae7876eb521ac47f4d3d814a894a344a9d716a
xhermitone <xhermitone@gmail.com>
2023-06-01 16:16:24 +0700

    Debuging

--- dbf_view_dos.py
+++ dbf_view_dos.py
@@ -11,11 +11,13 @@ import os.path
 import platform
 import getopt
 
-__version__ = (0, 0, 0, 1)
+__version__ = (0, 0, 0, 2)
 
-DOSBOX_CMD_FMT = 'dosbox -c \"mount C: %s\" -c \"mount D: /usr/share/dbf_view_dos\" -c \"D:\\rk.com\" -c "D:\\%s %s" %s &'
+DOSBOX_CMD_FMT = 'dosbox -c \"mount C: %s\" -c \"mount D: /usr/share/dbf_view_dos\" -c \"D:\\rk.com\" -c "D:\\%s %s" %s %s'
 
 DOSBOX_CMD_OPTION_EXIT = '-c "exit"'
+
+DOSBOX_CMD_OPTION_NOWAIT = '%s'
 
 DBF_VIEWERS = {
     'CDBF': 'CDBF\\cdbf.exe',
@@ -48,7 +50,8 @@ def main(*argv):
     try:
         options, args = getopt.getopt(argv, 'h?v',
                                       ['help', 'version',
-                                       'viewer=', 'dbf-filename=', 'no-exit'])
+                                       'viewer=', 'dbf-filename=',
+                                       'no-exit', 'no-wait'])
     except getopt.error as msg:
         print(str(msg))
         print(__doc__)
@@ -57,6 +60,7 @@ def main(*argv):
     viewer = 'CDBF'
     dbf_filename = None
     do_exit = True
+    do_wait = True
 
     for option, arg in options:
         if option in ('-h', '--help', '-?'):
@@ -72,14 +76,17 @@ def main(*argv):
             dbf_filename = arg
         elif option == '--no-exit':
             do_exit = False
+        elif option == '--no-wait':
+            do_wait = False
 
     if dbf_filename and os.path.exists(dbf_filename):
         viewer_option = DBF_VIEWERS.get(viewer, '')
         if viewer_option:
             home_path = getHomePath()
             exit_option = DOSBOX_CMD_OPTION_EXIT if do_exit else ''
+            wait_option = '' if do_wait else DOSBOX_CMD_OPTION_NOWAIT
             dos_dbf_filename = dbf_filename.replace(home_path, 'C:\\').replace(os.path.sep, '\\')
-            cmd = DOSBOX_CMD_FMT % (home_path, viewer_option, dos_dbf_filename, exit_option)
+            cmd = DOSBOX_CMD_FMT % (home_path, viewer_option, dos_dbf_filename, exit_option, wait_option)
             print(u'Run command <%s>' % cmd)
             os.system(cmd)
         else:
